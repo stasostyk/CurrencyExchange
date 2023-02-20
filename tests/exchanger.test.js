9cddd61cc602cd62722cd1435cfbb922e814b195
Staś Ostyk <s.ostyks@gmail.com>
2023-02-20 10:34:38 +0100

    fix test directory

--- tests/exchanger.test.js
+++ tests/exchanger.test.js
@@ -1,6 +1,6 @@
 // unit tests for the exchanging between currencies
 
-const {convertTo, convertFrom} = require("./public/exchanger.js");
+const {convertTo, convertFrom} = require("../public/exchanger.js");
 
 test("Check if GBP => PLN works properly", () => {
     expect(convertTo(5.5, 5)).toBe("1.10");
