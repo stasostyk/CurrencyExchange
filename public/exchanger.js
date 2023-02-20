ff2fb39567c6b9ccb785d3cb4a12cdac58834f6a
Staś Ostyk <s.ostyks@gmail.com>
2023-02-20 03:21:52 +0100

    API setup call

--- public/exchanger.js
+++ public/exchanger.js
@@ -1,13 +1,15 @@
 rate = NaN; // initialize as NaN so that if API fails, we simply do not exchange
 
-fetch("https://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json") // call the API for PLN => GBP rate
-  .then((response) => {response.json() // retrieve the json from the response
-    .then((data) => {
-      rate = data.rates[0].mid; // set rate to the real world rate from the API
-      document.getElementById("rate").innerHTML = `1 GBP = ${rate} PLN`; // communicate the rate on UI
-    })
-  });
-
+// call API and retrieve exchange rate (seperate function for unit testing simplicity)
+function ExchangeRateSetup() {
+  fetch("https://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json") // call the API for PLN => GBP rate
+    .then((response) => {response.json() // retrieve the json from the response
+      .then((data) => {
+        rate = data.rates[0].mid; // set rate to the real world rate from the API
+        document.getElementById("rate").innerHTML = `1 GBP = ${rate} PLN`; // communicate the rate on UI
+      })
+    });
+}
 
 // exchanges value from string from GBP to PLN using the NBP Web API (http://api.nbp.pl)
 async function exchange(val, toPLN) {
@@ -22,7 +24,7 @@ async function exchange(val, toPLN) {
 // converts a value from PLN to GBP
 function convertTo(num, rate) {
   var result = num / rate; // divide by exchange rate to convert GBP => PLN
-  if (isNan(result))
+  if (isNaN(result))
     return ""; // if the result is NaN, either rate failed to load or wrong input, hence return empty string
   else
     return result.toFixed(2); // return the applied rate to two decimals using JavaScript's 'toFixed()'
@@ -31,8 +33,10 @@ function convertTo(num, rate) {
 // converts a number from GBP to PLN
 function convertFrom(num, rate) {
   var result = num * rate; // multiply by exchange rate to convert PLN => GBP
-  if (isNan(result))
+  if (isNaN(result))
     return ""; // if the result is NaN, either rate failed to load or wrong input, hence return empty string
   else
     return result.toFixed(2); // return the applied rate to two decimals using JavaScript's 'toFixed()'
 }
+
+module.exports = {convertTo, convertFrom};
